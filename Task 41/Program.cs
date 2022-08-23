e14799329f386c7962bff99400e2f23e3c2a6c1c
AndrewDar <geologalone@gmail.com>
2022-08-23 18:24:11 +0300

    programs for hometask created

--- Task 41/Program.cs
+++ Task 41/Program.cs
@@ -2,6 +2,33 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3*/
 
+int CountPos(int[] arr)
+{
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0) count++;
+}
+return count;
+}
+
+
+/*Console.Write("Enter count of numbers: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
 Console.WriteLine("Enter numbers: ");
-Console.ReadLine();
+for (int i = 0; i < size; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int result = CountPos(array);
+Console.WriteLine($"The count of positive numbers: {result}");*/
+
+
+
+Console.Write("Enter numbers separated by spaces: ");
+string numbers = Console.ReadLine();
+int[] array = numbers.Split(' ').Select(Int32.Parse).ToArray();
+int result = CountPos(array);
+Console.WriteLine($"The count of positive numbers: {result}");
 
--- Task 43/Program.cs
+++ Task 43/Program.cs
@@ -1,6 +1,14 @@
 ﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)*/
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+
+void CrossPoint(double f1, double f2, double s1, double s2)
+{
+    double x = (s1 - f1) / (f2 - s2);
+    double y = s2 * x + s1;
+    Console.WriteLine($"Coordinates of the cross-point of two lines -> ({x}; {y})");
+}
 
 Console.Write("Enter number B1: ");
 double b1 = Convert.ToDouble(Console.ReadLine());
@@ -10,12 +18,6 @@ Console.Write("Enter number B2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Enter number K2: ");
 double k2 = Convert.ToDouble(Console.ReadLine());
+CrossPoint(b1, k1, b2, k2);
 
-void CrossPoint(double xa, double ya, double xb, double yb)
-{
-    double xc = (xb-xa)/(ya-yb);
-    double yc = (yb-ya)+xc+(xb-xa);
-    Console.WriteLine($"{xc}; {yc}");
-}
-CrossPoint(b1,k1,b2,k2);
 
