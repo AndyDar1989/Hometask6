834eafbfba9da2d2dcf208aa04bb05c7fe70f943
AndrewDar <geologalone@gmail.com>
2022-08-24 22:27:17 +0300

    warnings in task41 fixed

--- Task 41/Program.cs
+++ Task 41/Program.cs
@@ -1,15 +1,15 @@
 ﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 0, 7, 8, -2, -2 -> 2
-1, -7, 567, 89, 223-> 3*/
+1, -7, 567, 89, 223-> 4*/
 
 int CountPos(int[] arr)
 {
-int count = 0;
-for (int i = 0; i < arr.Length; i++)
-{
-    if (arr[i] > 0) count++;
-}
-return count;
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
 }
 
 
@@ -25,10 +25,19 @@ int result = CountPos(array);
 Console.WriteLine($"The count of positive numbers: {result}");*/
 
 
-
+void EnterNumbers()
+{
 Console.Write("Enter numbers separated by spaces: ");
-string numbers = Console.ReadLine();
-int[] array = numbers.Split(' ').Select(Int32.Parse).ToArray();
-int result = CountPos(array);
-Console.WriteLine($"The count of positive numbers: {result}");
+string? numbers;
+if ((numbers = Console.ReadLine()) != null)
+{
+    int[] array = numbers.Split(' ').Select(Int32.Parse).ToArray();
+    int result = CountPos(array);
+    Console.WriteLine($"The count of positive numbers: {result}");
+}
+}
+
+
+
+EnterNumbers();
 
